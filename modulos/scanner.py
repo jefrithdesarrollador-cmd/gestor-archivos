db446ce9703f6985e20d54f021a7223d6fe4d6d2
Jefrith Madariaga <jefrithdesarrollador@gmail.com>
2026-05-16 19:04:03 -0500

    Scanner con soporte de subcarpetas usando os.walk

--- modulos/scanner.py
+++ modulos/scanner.py
@@ -2,9 +2,9 @@ import os
 
 def listar_archivos(ruta, min_mb=0):
     archivos = []
-    for elemento in os.listdir(ruta):
-        ruta_completa = os.path.join(ruta, elemento)
-        if os.path.isfile(ruta_completa):
+    for carpeta, subcarpetas, ficheros in os.walk(ruta):
+        for fichero in ficheros:
+            ruta_completa = os.path.join(carpeta, fichero)
             tamanio_mb = os.path.getsize(ruta_completa) / (1024 * 1024)
             if tamanio_mb >= min_mb:
                 archivos.append(ruta_completa)
